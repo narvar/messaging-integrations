a6abff2ba1a402938ebb50c10e91ff96905106a3
Isha Singh <isha.singh@narvar.com>
2024-08-05 12:00:53 +0530

    initial commit

---
+++ src/main/java/com/narvar/messaging/integrations/MessagingIntegrationsApplication.java
@@ -0,0 +1,13 @@
+package com.narvar.messaging.integrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingIntegrationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingIntegrationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/narvar/messaging/integrations/MessagingIntegrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narvar.messaging.integrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingIntegrationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
